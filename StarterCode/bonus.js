b512fee84ce813dfe34273a8663a863caa12ac41
John Glen <jglendening11@gmail.com>
2021-05-02 21:45:42 -0500

    make it work

--- StarterCode/bonus.js
+++ StarterCode/bonus.js
@@ -7,15 +7,17 @@ var arrColorsG = ["#5899DA", "#E8743B", "#19A979", "#ED4A7B", "#945ECF", "#13A4B
 // tags for each key-value in the metadata.
 function buildMetadata(sample) {
     d3.json("samples.json").then((data) => {
-                var metadata = data.metadata;
-                var resultsarray = metadata.filter(sampleobject =>
-                    sampleobject.id == sample);
-                var result = resultsarray[0]
-                    // Use d3 to select the panel with id of `#sample-metadata`
-                var panel = d3.select("#sample-metadata");
-                // Use `.html("") to clear any existing metadata
-                panel.html("");
-                // Use `Object.entries` to add each key and value pair to the panel
-                Object.entries(result).forEach(([key, value]) => {
-                    panel.append("h6").text(`${key}: ${value}`);
-                });
+        var metadata = data.metadata;
+        var resultsarray = metadata.filter(sampleobject =>
+            sampleobject.id == sample);
+        var result = resultsarray[0]
+            // Use d3 to select the panel with id of `#sample-metadata`
+        var panel = d3.select("#sample-metadata");
+        // Use `.html("") to clear any existing metadata
+        panel.html("");
+        // Use `Object.entries` to add each key and value pair to the panel
+        Object.entries(result).forEach(([key, value]) => {
+            panel.append("h6").text(`${key}: ${value}`);
+        });
+    });
+}
