20f51b7f1268a61003b36123ba748a0365b21ae0
John Glen <jglendening11@gmail.com>
2021-05-02 21:58:32 -0500

    updates

--- StarterCode/bonus.js
+++ StarterCode/bonus.js
@@ -170,3 +170,39 @@ function buildCharts(sample) {
         Plotly.newPlot("bar", bar_data, barLayout);
     });
 }
+function init() {
+// Grab a reference to the dropdown select element
+var selector = d3.select("#selDataset");
+
+// Use the list of sample names to populate the select options
+d3.json("samples.json").then((data) => {
+  var sampleNames = data.names;
+  sampleNames.forEach((sample) => {
+    selector
+      .append("option")
+      .text(sample)
+      .property("value", sample);
+  });
+
+  // Use the first sample from the list to build the initial plots
+  const firstSample = sampleNames[0];
+  buildMetadata(firstSample);
+  buildCharts(firstSample);
+  buildGaugeChart(firstSample)
+
+
+});
+}
+
+function optionChanged(newSample) {
+// Get new data each time a new sample is selected
+buildMetadata(newSample);
+buildCharts(newSample);
+buildGaugeChart(newSample)
+
+}
+
+
+
+// Initialize the dashboard
+init();
