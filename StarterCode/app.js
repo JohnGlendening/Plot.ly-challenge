f609eda31d918eee6ff89d0c8922101d282bd77c
John Glen <jglendening11@gmail.com>
2021-05-02 21:33:29 -0500

    edit

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -4,12 +4,14 @@
 
 function buildMetadata(sample) {
     d3.json("samples.json").then((data) => {
-                var metadata = data.metadata;
-                var resultsarray = metadata.filter(sampleobject =>
-                    sampleobject.id == sample);
-                var result = resultsarray[0]
-                var panel = d3.select("#sample-metadata");
-                panel.html("");
-                Object.entries(result).forEach(([key, value]) => {
-                    panel.append("h6").text(`${key}: ${value}`);
-                });
+        var metadata = data.metadata;
+        var resultsarray = metadata.filter(sampleobject =>
+            sampleobject.id == sample);
+        var result = resultsarray[0]
+        var panel = d3.select("#sample-metadata");
+        panel.html("");
+        Object.entries(result).forEach(([key, value]) => {
+            panel.append("h6").text(`${key}: ${value}`);
+        });
+    });
+}
